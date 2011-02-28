24ab45c76876b744d3d8b0949fc983c21d94b880
Tom von Schwerdtner <tomvons@gmail.com>
2011-02-28 02:01:10 -0500

    a few helper template tags

---
+++ templatetags/page_helpers.py
@@ -0,0 +1,27 @@
+from mezzanine.pages.models import Page
+
+from mezzanine import template
+
+register = template.Library()
+
+
+@register.as_tag
+def page_by_slug(slug):
+    """
+    Return a page matching the provided slug, eg 'index' or 'about/team'
+    """
+    try:
+        return Page.objects.get(slug=slug)
+    except Page.DoesNotExist:
+        return None
+
+
+@register.as_tag
+def page_by_id(id):
+    """
+    Return a page matching the provided slug, eg 'index' or 'about/team'
+    """
+    try:
+        return Page.objects.get(pk=id)
+    except Page.DoesNotExist:
+        return None
