5bd10865b30bb34830883e6caa2239101c63613c
Tom von Schwerdtner <tomvons@gmail.com>
2011-03-01 16:45:29 -0500

    fix by_id docstring

--- templatetags/page_helpers.py
+++ templatetags/page_helpers.py
@@ -19,7 +19,7 @@ def page_by_slug(slug):
 @register.as_tag
 def page_by_id(id):
     """
-    Return a page matching the provided slug, eg 'index' or 'about/team'
+    Return a page by id
     """
     try:
         return Page.objects.get(pk=id)
